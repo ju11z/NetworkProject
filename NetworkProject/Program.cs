8e49e95de3dfa5e1bab1f9b856485b874c313066
ju11z <fgtmnt@mail.ru>
2024-10-06 11:44:35 +0300

    Add project files.

---
+++ NetworkProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NetworkProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
